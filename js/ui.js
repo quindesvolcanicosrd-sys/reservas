c60203646fa6ef1598da2aa7ee8d483a77e92a97
quindesvolcanicosrd-sys <quindesvolcanicosrd@gmail.com>
2026-06-25 22:47:58 -0500

    comit

--- js/ui.js
+++ js/ui.js
@@ -65,7 +65,7 @@ var TOP_BAR_CONFIG = {
   's2': { titulo: 'Equipamiento', volver: 's-home' },
   's3a': { titulo: 'Editar equipamiento', volver: function() { return E.editandoDesdeHome ? 's-home' : 's2'; } }, 's3b': { titulo: 'Editar equipamiento', volver: 's3a' },
   's3c': { titulo: 'Editar equipamiento', volver: function() { return E.editPat === 'Sí' ? 's3b' : 's3a'; } },
-  's4': { titulo: function() { return E.reagendando ? 'Reagendar clase' : canPayMonthly() ? 'Selecciona el tipo de modalidad' : 'Fecha y pago'; }, volver: function() { return (E.reagendando || canPayMonthly()) ? 's-home' : 's2'; } },
+  's4': { titulo: function() { return E.reagendando ? 'Reagendar clase' : canPayMonthly() ? 'Nueva reserva' : 'Fecha y pago'; }, volver: function() { return (E.reagendando || canPayMonthly()) ? 's-home' : 's2'; } },
   's-pago': { titulo: 'Pago', volver: 's4' }, 's5': { titulo: 'Confirmar reserva', volver: function() { return (E.creditosUsados > 0 || E.cuponAplicado) && E.totalPago === 0 ? 's4' : 's-pago'; } },
   's-misreservas': { titulo: 'Historial de reservas', volver: 's-home' }, 's-datos': { titulo: 'Editar mis datos', volver: 's-home' },
   's-admin-login': { titulo: 'Administradorx', volver: 's1' },
