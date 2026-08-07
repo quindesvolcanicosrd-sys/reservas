b4b481bdd449843bcdb46ea0b707b79dc7f46ba5
quindesvolcanicosrd-sys <quindesvolcanicosrd@gmail.com>
2026-08-07 11:37:29 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-var BACKEND = 'https://script.google.com/macros/s/AKfycbwz0nvYgASPv0aLNFrXx6_9W3_lev2X_scy0kKnk08tB6zdZIVxMcPjT9ZKye1V1aVA/exec';
+var BACKEND = 'https://script.google.com/macros/s/AKfycbxw7KT8OBm6ZeBcVu3J7WILCR8-lsSJ1VQxEP6yc2OGfXxLI3T9hGcwERIb_3n281mu/exec';
 var GOOGLE_CLIENT_ID = '632992894668-gnbb5cclsmfdcnve0g34kmue1c72h73q.apps.googleusercontent.com';
 var MAPS_API_KEY = 'AIzaSyDSGkh2AyuM_-6ngo5-XMPi0NYrXZHEBl8';
 
