9d56bdebacc0491d9c31a71151c0befb35cd1e77
quindesvolcanicosrd-sys <quindesvolcanicosrd@gmail.com>
2026-08-13 19:16:18 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://uusbnreitoobqssizbfq.supabase.co/functions/v1/api';
+var BACKEND = 'https://uusbnreitoobqssizbfq.supabase.co/functions/v1/api';
 var GOOGLE_CLIENT_ID = '632992894668-gnbb5cclsmfdcnve0g34kmue1c72h73q.apps.googleusercontent.com';
 var MAPS_API_KEY = 'AIzaSyDSGkh2AyuM_-6ngo5-XMPi0NYrXZHEBl8';
 
