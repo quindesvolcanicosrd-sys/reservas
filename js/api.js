5a97e970d610dfca98ea57f693b9dbe6fa0b113a
quindesvolcanicosrd-sys <quindesvolcanicosrd@gmail.com>
2026-08-17 11:02:49 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ var _token = '';
 function api(params, onSuccess, onError) {
   if (_token && !params.token) params.token = _token;
   var url = BACKEND + '?' + Object.keys(params).map(function(k) { return encodeURIComponent(k) + '=' + encodeURIComponent(params[k]); }).join('&');
-  fetch(url, { method: 'GET', mode: 'cors' })
+  fetch(url, { method: 'GET', mode: 'cors', headers: { 'apikey': SUPABASE_ANON_KEY, 'Authorization': 'Bearer ' + SUPABASE_ANON_KEY } })
     .then(function(r) { return r.json(); })
     .then(function(data) { if (data && data.error) { onError({ message: data.error }); } else { onSuccess(data); } })
     .catch(function(e) { onError(e); });
@@ -15,7 +15,7 @@ function apiPost(params, onSuccess, onError) {
   }).join('&');
   fetch(BACKEND, {
     method: 'POST', mode: 'cors',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'apikey': SUPABASE_ANON_KEY, 'Authorization': 'Bearer ' + SUPABASE_ANON_KEY },
     body: body
   })
   .then(function(r) { return r.json(); })
